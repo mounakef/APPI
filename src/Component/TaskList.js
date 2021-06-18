948424e23313ed55405a1c25095b417e3caa5a26
full-stack-js-dev <baltidanger@live.fr>
2021-06-18 12:51:27 +0100

    first commit

--- src/Component/TaskList.js
+++ src/Component/TaskList.js
@@ -1,17 +1,48 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { useSelector } from "react-redux";
-
 import Task from './Task'
 
 const TaskList = () => {
-    const toDos = useSelector(state => state.toDo)
+    const toDos = useSelector((state) => state.toDo)
+    const [safwen, setSafwen] = useState("all")
+   
+
+    const modifList =()=>{
+     if(safwen==="all"){
+       
+       return toDos
+     }
+else if(safwen==="done"){
+    return toDos.filter (el=>el.isDone==true)
+         }
+         else if(safwen==="undone"){
+          return  toDos.filter (el=>el.isDone==false)        
+         }
+        }
+let List = modifList()
+
+    
+const handleAll =()=>{ 
+  
+    return setSafwen("all")
+    
+ }
+const handleDone =()=>{ 
+   return  setSafwen("done")
+}
+const handleUndone=()=>{ 
+    return setSafwen("undone")
+}
+
     return (
         <div>
-        
-        { toDos.map(el=>
-            <Task element={el}  key={el.id}/>
-            ) }
-        
+        <button onClick= {handleAll}> All</button>
+        <button onClick ={handleDone}> done </button>
+        <button onClick ={handleUndone}> undone</button>
+        <div>
+        { List.map(el=>{
+        return <Task element={el}  key={el.id}/>}) }
+        </div>
         </div>
     )
 }
